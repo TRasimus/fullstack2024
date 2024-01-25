d6a397d40b0560523b1909d59f317cd438a21526
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-01-25 16:23:01 +0200

    Tehtävä 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -13,19 +13,25 @@ const Statistics = ({good, neutral, bad}) => {
   const positive = (good / all) * 100
 
   return (
-    <div>
+
+  <table>
+    <tbody>
       <StatisticLine text="good" value ={good} />
       <StatisticLine text="neutral" value ={neutral} />
       <StatisticLine text="bad" value ={bad} />
       <StatisticLine text="all" value ={all} />
       <StatisticLine text="average" value ={average} />
       <StatisticLine text="positive" value ={positive} />
-    </div>
+    </tbody> 
+  </table>
   )
 }
 
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text} </td>
+    <td>{value} </td>
+  </tr>
 )
 
 const Button = (props) => (
