4edf53bcd364f6eeb63136e790f2f0de48c5b8a5
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-01-25 13:31:01 +0200

    Tehtävä 1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const Statistics = ({good, neutral, bad}) => (
+  <div>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </div>
+)
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadFeedback = () => { 
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodFeedback} text="good" />
+      <Button handleClick={handleNeutralFeedback} text="neutral" />
+      <Button handleClick={handleBadFeedback} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
