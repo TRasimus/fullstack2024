037621b9b950c61748d7100feb988a8440dac04a
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-02-01 21:54:57 +0200

    Tehtävä 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
 
@@ -52,19 +55,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with<input value={filter} onChange={handleFilterChange}/></div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>name:<input value={newName} onChange={handleNameChange}/></div>
-        <div>number:<input value={newNumber} onChange={handleNumberChange}/></div>
-        <button type="submit">add</button>
-      </form>   
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
-      </ul>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <h3>add a new</h3>
+      <PersonForm 
+        addPerson={addPerson} 
+        newName={newName} 
+        handleNameChange={handleNameChange}
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange}
+        />
+      <h3>Numbers</h3>
+      <Persons personsToShow= {personsToShow}/>
     </div>
   )
 }
