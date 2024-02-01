82f986433e618344c97d3a6d3fcdc57b93e7c003
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-02-01 21:33:06 +0200

    Tehtävä 2.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,10 +3,23 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [personsToShow, setPersonsToShow] = useState(persons)
+
+  const handleFilterChange = (event) => {
+    const newFilter = event.target.value
+    setFilter(newFilter)
+    const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(newFilter.toLowerCase()))
+    setPersonsToShow(filteredPersons)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -28,15 +41,19 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-    setPersons(persons.concat(personObject))
+    const updatedPersons = persons.concat(personObject)
+    setPersons(updatedPersons)
     setNewName('')
     setNewNumber('')
+    setPersonsToShow(updatedPersons)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with<input value={filter} onChange={handleFilterChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name:<input value={newName} onChange={handleNameChange}/></div>
         <div>number:<input value={newNumber} onChange={handleNumberChange}/></div>
@@ -44,7 +61,7 @@ const App = () => {
       </form>   
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {personsToShow.map(person => 
           <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
