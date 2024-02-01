7b13b94435d498e43962882d4515eb492f4b227d
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2024-02-01 15:22:59 +0200

    Tehtävä 2.1

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,81 @@
+
+const Header = ({name}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <div>
+       <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => 
+          <Part key={part.id} part={part} />
+        )}
+    </div>
+  )
+}
+
+/*
+const Total = ({parts}) => {
+  const total = Number(parts[0].exercises + parts[1].exercises + parts[2].exercises)
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+
+*/
+
+const Course = ({course}) => {
+  return (
+    <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
